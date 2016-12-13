6776df4d4b99447e65bcd86e06820baccc3e3a65
denis-torre <denis.torre@mssm.edu>
2016-12-13 18:41:29 -0500

    Redesigned Website From Scratch, Added API

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,32 +12,27 @@
 ##### 1.1 Load Libraries
 ##############################
 # Python Libraries
-import sys
+import sys, urlparse
 import pandas as pd
 
 # Flask Libraries
 from flask import Flask, request, render_template
-from flaskext.mysql import MySQL
 
 # Custom libraries
 sys.path.append('static/lib')
 from lib import *
 
 ##############################
-##### 1.2 Setup MySQL
+##### 1.2 Setup App
 ##############################
 # Initialize Flask App
 app = Flask(__name__)
 
-# Initialize MySQL Connection
-mysql = MySQL()
+# Get Connection File
+databaseConnectionFile = 'static/dbconnection/dbconnection.json'
 
-# Configure MySQL Connection
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'MyNewPass'
-app.config['MYSQL_DATABASE_DB'] = 'datasets2tools'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+# Connect to MySQL
+app, mysql = setupMySQLConnection(app, databaseConnectionFile, 'local')
 
 #######################################################
 ########## 2. Setup Web Page ##########################
@@ -50,23 +45,19 @@ mysql.init_app(app)
 ### 2.1.1 Main
 @app.route('/')
 def main():
-	return render_template('index.html')
+	return 'hello'
 
-### 2.1.2 Save Canned Analysis
-@app.route('/save')
-def save():
-	# Query tools
-	tool_dataframe = executeQuery("SELECT * FROM tool", mysql)
-	return render_template('save.html', tool_dataframe=tool_dataframe)
-
-### 2.1.3 Dataset Search
-@app.route('/datasetSearch', methods=['POST'])
-def datasetSearch():
-	dataset_search_query = request.form['dataset_search_query']
-	return dataset_search_query
-
-
-
+##############################
+##### 2.2 API
+##############################
+### 2.2.1
+@app.route('/data')
+def data():
+	if '?' in request.url:
+		datasetAccessions = request.url.split('?')[1].split('+')
+		return mainAPI(datasetAccessions, mysql)
+	else:
+		return 'data'
 
 #######################################################
 ########## 3. Run Flask App ###########################
